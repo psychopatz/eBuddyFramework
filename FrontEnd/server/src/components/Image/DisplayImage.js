52a32c7622df87bce5fa0dc299ce77ec950b9d37
Psychopatz <bpatrickoliverb@gmail.com>
2024-06-01 22:15:44 +0800

    Implement Placeholder for error photos

--- FrontEnd/server/src/components/Image/DisplayImage.js
+++ FrontEnd/server/src/components/Image/DisplayImage.js
@@ -10,15 +10,21 @@ const DisplayImage = ({ imageUrls, onImageClick, style }) => {
         ...style  // Merge custom style with default style
     };
 
+    // Function to handle image loading errors
+    const handleImageError = (e) => {
+        e.target.src = "/imagePlaceholder.jpeg"; // Set placeholder image on error, assuming it's in the public folder
+    };
+
     return (
         <>
-            {imageUrls.map(url => (
+            {imageUrls.map((url, index) => (
                 <img
-                    key={url}
+                    key={index}  // Using index as key because URL may not be unique
                     src={url}
-                    alt="Image Not Found"
+                    alt="Photo"
                     style={defaultStyle}
                     onClick={() => onImageClick(url)}
+                    onError={handleImageError}  // Add onError handler to replace the src with placeholder image
                 />
             ))}
         </>
